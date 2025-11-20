5530adf19a2d7c0f668fffce60e37c46886a66fa
mayuripatil5111-cmd <mayuripatil5111@gmail.com>
2025-11-20 20:46:08 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
  <footer class="foo">
             <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:17px;"><i class="material-icons" style="color: brown;">COPYRIGHT</i> 2018-19 <b >Online Notes Sharing <a href="https://projectworlds.in">Projectworlds</a></b></p>
+                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:17px;"><i class="material-icons" style="color: brown;"><b >Online Skill Exchange Notes Sharing </b></p>
                 </div>
             </div>
    </footer>
