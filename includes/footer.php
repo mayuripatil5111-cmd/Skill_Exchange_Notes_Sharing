3ab47bb32f21f8a9791d11f132c07b91904f43e5
mayuripatil5111-cmd <mayuripatil5111@gmail.com>
2025-11-20 20:47:53 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
  <footer class="foo">
             <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:17px;"><i class="material-icons" style="color: brown;"><b >Online Skill Exchange Notes Sharing </b></p>
+                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:17px;"><i class="material-icons" style="color: brown;"></p>
                 </div>
             </div>
    </footer>
