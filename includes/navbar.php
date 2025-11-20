bcf9f73846213e02453b9264642d56df92c4e62a
mayuripatil5111-cmd <mayuripatil5111@gmail.com>
2025-11-20 20:48:57 +0530

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,7 +9,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="index.php">College-Notes-Gallery</a>
+					<a class="navbar-brand" href="index.php">Skill_Exchange_Notes_Sharing</a>
 				</div>
 
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
