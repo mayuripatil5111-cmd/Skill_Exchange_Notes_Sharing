9a4b9b7ac9703c4688966ed38ca0f0fd6d6ff597
mayuripatil5111-cmd <mayuripatil5111@gmail.com>
2025-11-20 21:13:58 +0530

    Update upload.php

--- dashboard/upload.php
+++ dashboard/upload.php
@@ -15,23 +15,23 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            UPLOAD NOTE
+                            UPLOAD Skill and NOTE
                         </h1>
                         <form role="form" action="" method="POST" enctype="multipart/form-data">
 
 
 	<div class="form-group">
-		<label for="post_title">Note Title</label>
+		<label for="post_title">Skill or Note Title</label>
 		<input type="text" name="title" class="form-control" placeholder="Eg: Php Tutorial File" ">
 	</div>
 
 	<div class="form-group">
-		<label for="post_tags">Note Tags</label>
+		<label for="post_tags">Skill or Note Tags</label>
 		<input type="text" name="tags" class="form-control" placeholder="Eg: Php, Tutorial, Learn Php etc.." ">
 	</div>
 
 	<div class="form-group">
-		<label for="post_content">Write a short Description about your note</label>
+		<label for="post_content">Write a short Description about your skill or note</label>
 		<textarea  class="form-control" name="description" id="" cols="12" rows="6">
 		</textarea>
 	</div>
@@ -41,7 +41,7 @@
 		<input type="file" name="file"> 
      </div>
 
-<button type="submit" name="upload" class="btn btn-primary" value="Upload Note">Upload Note</button>
+<button type="submit" name="upload" class="btn btn-primary" value="Upload Note">Upload skill or Note</button>
 <br>
 <br>
 </form>
