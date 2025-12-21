cbc2b6b0f7048ed291e7a85d0dbc8fda8f6e0e35
mayuripatil5111-cmd <mayuripatil5111@gmail.com>
2025-12-21 20:01:34 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,50 @@
- <footer class="foo">
-            <div class="row">
-                <div class="col-lg-12">
-                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:17px;"><i class="material-icons" style="color: brown;"></p>
-                </div>
-            </div>
-   </footer>
+
+<link rel="stylesheet" href="css/home.css">
+
+<footer class="footer">
+
+  <div class="footer-container">
+
+    <!-- ABOUT -->
+    <div class="footer-col">
+      <h4>About Platform</h4>
+      <p>
+        A smart educational platform designed to help students and teachers
+        share notes, exchange skills, and collaborate effectively.
+      </p>
+    </div>
+
+    <!-- QUICK LINKS -->
+    <div class="footer-col">
+      <h4>Quick Links</h4>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="signup.php">Sign Up</a></li>
+        <li><a href="dashboard/">Dashboard</a></li>
+      </ul>
+    </div>
+
+    <!-- CONTACT -->
+    <div class="footer-col">
+      <h4>Contact</h4>
+      <p>Email: support@learningplatform.com</p>
+      <p>Phone: +91 XXXXX XXXXX</p>
+
+      <!-- SOCIAL ICONS -->
+      <div class="footer-social">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="#"><i class="fa fa-github"></i></a>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- BOTTOM -->
+  <div class="footer-bottom">
+    © <?php echo date("Y"); ?> Student Learning Platform | All Rights Reserved
+  </div>
+
+</footer>
